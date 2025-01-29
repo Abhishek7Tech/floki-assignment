2bd624f15085dd4c8c0e72a2a2da20a63eb2f29a
Abhishek7Tech <abhishektab45@gmail.com>
2025-01-29 15:23:53 +0530

    implement signin, disconnect functionality

--- config.ts
+++ config.ts
@@ -1,12 +1,11 @@
 import { http, createConfig } from "wagmi";
 import { mainnet, sepolia, bsc } from "wagmi/chains";
-import {  metaMask, walletConnect } from "wagmi/connectors";
-
-const projectId = "";
+import { metaMask, walletConnect } from "wagmi/connectors";
+const projectId = import.meta.env.VITE_PROJECT_ID || "";
 
 export const config = createConfig({
   chains: [mainnet, sepolia, bsc],
-  connectors: [ walletConnect({ projectId }), metaMask()],
+  connectors: [walletConnect({ projectId }), metaMask()],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
--- src/components/sign-message.tsx
+++ src/components/sign-message.tsx
@@ -6,38 +6,105 @@ import {
   SelectValue,
   SelectTrigger,
 } from "@/components/ui/select";
-import { useAccount, useChains } from "wagmi";
+import {
+  useAccount,
+  useChains,
+  useDisconnect,
+  useSignMessage,
+  useSwitchChain,
+} from "wagmi";
 import { config } from "../../config";
 import Eth from "@/assets/eth.svg";
 import BNB from "@/assets/bnb.svg";
 import Synergy from "@/assets/Synergy.svg";
+import { Button } from "./ui/button";
+import Logout from "@/assets/logout.svg";
+import { getBalance, GetBalanceReturnType } from "@wagmi/core";
+import { useEffect, useState } from "react";
+import { recoverMessageAddress } from "viem";
 export function SignMessage() {
-  const chains = useChains({
-    config,
-  });
+  const { chains, switchChain } = useSwitchChain();
+  const [balance, setBalance] = useState<GetBalanceReturnType | null>(null);
+  const { disconnect } = useDisconnect();
+  const [recoveredAddress, setRecoveredAddress] = useState("");
+  const {
+    data: signMessageData,
+    error,
+    signMessage,
+    isPending,
+    isSuccess,
+    variables,
+  } = useSignMessage();
+  // const chains = useChains({
+  //   config,
+  // });
 
+  // useEffect(() => {
+  //   console.log("Data", signMessageData, error, isSuccess, isPending);
+  // }, [signMessageData ]);
+
+  // const [chain, setChain] = useState<string | undefined>(undefined);
   const account = useAccount();
+
+  useEffect(() => {
+    (async () => {
+      if (variables?.message && signMessageData) {
+        const recoveredAddress = await recoverMessageAddress({
+          message: variables?.message,
+          signature: signMessageData,
+        });
+        setRecoveredAddress(recoveredAddress);
+      }
+    })();
+  }, [account, signMessageData, variables?.message]);
+
+  async function signMessageHandler() {
+    signMessage({ message: "hello world!" });
+  }
+
+  async function getAccountBalance(address: `0x${string}` | undefined) {
+    if (!address) {
+      return;
+    }
+    const balance = await getBalance(config, {
+      address: address,
+    });
+    setBalance(balance);
+    console.log("Bal", balance);
+    return balance;
+  }
+
   const sliceAccountAddress =
     account.address?.slice(0, 4) +
     "..." +
     account.address?.slice(account.address.length - 4, account.address.length);
+
+  const chainHandler = (chainId: { chainId: number }) => {
+    switchChain(chainId);
+    console.log("Chain", chainId);
+    getAccountBalance(account.address);
+  };
   return (
-    <div>
+    <div className="flex gap-4">
       <Select>
         <SelectTrigger className="w-[180px] bg-[#F2F5F8] rounded-[10px]">
           <SelectValue placeholder="Select a blockchain."></SelectValue>
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
-            {chains.map((chain) => {
+            {chains.map((chain, idx) => {
               return (
-                <SelectItem value={chain.name}>
-                  <div className="flex gap-2 font-medium text-sm items-center">
+                <SelectItem
+                  onClick={() => chainHandler({ chainId: chain.id })}
+                  value={chain.name}
+                  key={idx}
+                >
+                  <div className="flex gap-2 font-medium text-sm items-center chain-div">
                     <img
                       src={chain.name !== "BNB Smart Chain" ? Eth : BNB}
                       alt={`${chain.name} logo`}
                     ></img>
-                    {chain.name}
+                    <span>{chain.name}</span>
                   </div>
                 </SelectItem>
               );
@@ -50,14 +117,36 @@ export function SignMessage() {
         </SelectContent>
       </Select>
 
-      <div>
+      <div className="flex flex-col gap-2">
         <Select>
-          <SelectTrigger className="w-[233px] bg-[#F2F5F8] rounded-[10px]">
-            <span>0.25 ETH</span>
+          <SelectTrigger className="p-4 bg-[#F2F5F8] rounded-[10px]">
+            <span>
+              {balance
+                ? balance.value.toString().slice(0, 3) + " " + balance.symbol
+                : "0.00 BAL"}
+            </span>
             <img src={Synergy} alt="wallet logo"></img>
             <span>{sliceAccountAddress}</span>
           </SelectTrigger>
         </Select>
+
+        <div className="bg-[#F2F5F8] rounded-[10px] flex flex-col gap-2 p-4">
+          <Button
+            onClick={() => signMessageHandler()}
+            className="w-[210px] rounded-[10px]"
+          >
+            Sign Message
+          </Button>
+          <Button
+            onClick={() => disconnect()}
+            variant="outline"
+            className="w-[210px] flex gap-1 rounded-[10px]"
+          >
+            <img src={Logout} alt="disconnect button icon"></img>{" "}
+            <span>Disconnect</span>
+          </Button>
+        </div>
+        <h1>{recoveredAddress}</h1>
       </div>
     </div>
   );
